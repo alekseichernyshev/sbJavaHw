3aba744692edd277f5f254a00d3f599582d11053
alekseichernyshev <adanayrel@mail.ru>
2022-12-13 22:30:38 +0900

    Первая задача

---
+++ src/HW1/Task1.java
@@ -0,0 +1,16 @@
+package HW1;
+
+import java.util.Scanner;
+
+/*
+Вычислите и выведите на экран объём шара, получив его радиус с консоли
+ */
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        double r = console.nextDouble();
+
+        System.out.println(4. / 3 * Math.PI * Math.pow(r, 3) );
+
+    }
+}
