5a2f40906a36ac7a3f3bed9ddcc2f18f4b151519
alekseichernyshev <adanayrel@mail.ru>
2022-12-13 22:52:59 +0900

    Третья задача

---
+++ src/HW1/Task3.java
@@ -0,0 +1,13 @@
+package HW1;
+
+import java.util.Scanner;
+
+public class Task3 {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        String userName = console.next();
+        System.out.println("Привет," + userName + "!");
+
+
+    }
+}
